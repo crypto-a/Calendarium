703421240936948e983e83bc7df2a57b28a0be19
Benjamin <Benjamin.gavriely@gmail.com>
2023-12-23 12:55:21 -0500

    Whats up with this

--- calendarium_backend/Authentication/API/APIUserSignUp.py
+++ calendarium_backend/Authentication/API/APIUserSignUp.py
@@ -6,5 +6,14 @@ authentication = Authentication()
 
 
 class APIUserSignUp(Resource):
-    def get(self):
-        return "Hello World"
+    def post(self):
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        username = request.args.get('username')
+        email = request.args.get('email')
+        password = request.args.get('password')
+
+        #Create a new User
+        authentication.user_signup(first_name, last_name, username, email, password)
+
+        return 201
--- calendarium_backend/Authentication/Authentication.py
+++ calendarium_backend/Authentication/Authentication.py
@@ -1,3 +1,20 @@
+import models
+
+from calendarium_backend.database.models import User
+
 
 class Authentication:
+
+    def user_signup(self, first_name, last_name, username, email, password):
+        """
+        Programmer: Benjamin Gavriely
+        Date: December 23, 2023
+        Creates a new user with the sign up information
+        """
+        return User(first_name=first_name,
+                        last_name=last_name,
+                        username=username,
+                        email=email,
+                        password=password)
+
     pass
