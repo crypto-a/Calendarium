92c5d2e3b2f2ea18af61ee3d9f75cb27f97e81c1
crypto-a <alirahbar2005@gmail.com>
2023-12-27 13:52:33 -0500

    Updated requirements.txt

--- calendarium_backend/Calendarium/Actions/Base/CalendarAccount.py
+++ calendarium_backend/Calendarium/Actions/Base/CalendarAccount.py
@@ -3,4 +3,18 @@
 class CalendarAccount:
     _username: str
     _password: str
+    def __init__(self, username: str, password: str):
+        self._username = username
+        self._password = password
 
+    def read_events_from_calendar(self):
+        raise NotImplementedError
+
+    def add_event_to_calendar(self):
+        raise NotImplementedError
+
+    def delete_event_from_calendar(self):
+        raise NotImplementedError
+
+    def keep_track_of_calendar(self):
+        raise NotImplementedError
